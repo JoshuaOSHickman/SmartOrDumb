2b18d618aed71eef31865778915fbf5a32a3ba75
Joshua Orion Skylar Hickman <IRespondPromptlyWhenContacted@gmail.com>
2013-05-05 11:13:28 -0700

    init (rails)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmartOrDumb::Application.config.secret_token = '19a7dfc4f307df64cc62123b271cb9d68789648335a8c21c522d3be217c2723351431bac3fec104fff3a17beb379e1634903e70f8574277afcffc54a6536ceba'
